32e1bed1e25cf1386d14189f2a836ab8a4a5b026
“preferredintcom” <olawale.olasupo@preferredintegratedservices.pl>
2025-03-30 08:31:19 +0200

    tfplan

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  cloud { 
+    
+    organization = "azureservicestask" 
+
+    workspaces { 
+      name = "terrabeast" 
+    } 
+  } 
+  
+}
 provider "azurerm" {
   features {}
   
